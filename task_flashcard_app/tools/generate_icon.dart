68d0eea134805db3ac142c2afe1c8c27b0bd194c
Gunner Robinson <gunnerwrobinson@gmail.com>
2025-05-19 23:51:05 +0100

    apple build changes

--- task_flashcard_app/tools/generate_icon.dart
+++ task_flashcard_app/tools/generate_icon.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:path_provider/path_provider.dart';
 
 void main() async {
   final recorder = ui.PictureRecorder();
